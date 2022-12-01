a7ddba25698b1ec49ec0d084505739cbbcb80d93
Zack Katz <zack@katz.co>
2022-12-01 09:40:53 -0500

    Fix help desk appearance

--- src/components/IntegrationSettings.js
+++ src/components/IntegrationSettings.js
@@ -7,7 +7,7 @@ const IntegrationSettings = () => {
   return (
     <div className="flex flex-col px-5 py-6 sm:px-10">
       <PageHeader title={"Integrations"} subTitle={"Manage Integrations"} />
-      <div className="flex flex-col justify-center w-full bg-white rounded-lg shadow">
+      <div className="flex flex-col justify-center w-full bg-white rounded-lg shadow p-8">
         <ul
           role="list"
           className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
