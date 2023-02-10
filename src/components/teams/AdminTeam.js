aaaf26ee9767067e42f8babd76e2b1b4c7f0d040
Josh Pollock <jpollock412@gmail.com>
2023-02-10 17:37:29 -0500

    Login form when not authenticated

--- src/components/teams/AdminTeam.js
+++ src/components/teams/AdminTeam.js
@@ -6,12 +6,14 @@ import { CenteredLayout, NarrowFormLayout } from "../Layout";
 import TitleDescriptionLink from "../TitleDescriptionLink";
 import { PrimaryButton } from "../Buttons";
 import { __ } from "@wordpress/i18n";
+import Modal from "../Modal";
+import LoginOrLogout from "../LoginLogout";
 
-const InviteMember = () => {
+const InviteMember = ({ team }) => {
   const handler = (e) => {
     e.preventDefault();
     const email = e.target.email.value;
-    console.log({ email });
+    console.log({ email, team: team.account_id });
   };
   return (
     <NarrowFormLayout
@@ -20,10 +22,11 @@ const InviteMember = () => {
       description={__("Lorem Ipsum", "trustedlogin-vendor")}>
       <>
         <form
+          onSubmit={handler}
           aria-label={__("Invite New Team Member", "trustedlogin-vendor")}
           className="flex flex-col py-6 space-y-6 justify-center">
           <input name={"email"} type={"email"} placeholder={"Email Address"} />
-          <PrimaryButton type={"submit"}>
+          <PrimaryButton type={"submit"} onClick={handler}>
             {__("Invite", "trustedlogin-vendor")}
           </PrimaryButton>
         </form>
@@ -31,11 +34,15 @@ const InviteMember = () => {
     </NarrowFormLayout>
   );
 };
+
 export default function AdminTeam() {
   const { currentTeam } = useView();
+  //should show login form?
+  const [showLogin, setShowLogin] = useState(false);
   //track state for modal open/close
-  const [modalOpen, setModalOpen] = useState(false);
+  const [modalOpen, setModalTeam] = useState(false);
   const [members, setMembers] = useState([]);
+  const [hasLoaded, setHasLoaded] = useState(false);
   const items = useMemo(() => {
     if (members.length > 0) {
       return members.map((member) => {
@@ -51,6 +58,26 @@ export default function AdminTeam() {
     return [];
   }, [members]);
 
+  const handleProxyResponse = (r) => {
+    hasLoaded(true);
+
+    if (r.code) {
+      switch (r.code) {
+        case 401:
+          setShowLogin(true);
+          break;
+
+        default:
+          //nothing
+          break;
+      }
+    }
+    if (r.data) {
+      return r.data;
+    }
+    return r;
+  };
+
   useEffect(() => {
     if (currentTeam < 1) return;
     fetchWithProxyRoute({
@@ -62,44 +89,68 @@ export default function AdminTeam() {
       type: "teams",
     })
       .then((r) => {
-        console.log({ r });
-        setMembers(r.data);
+        let data = handleProxyResponse(r);
+        if (data) {
+          setMembers(data);
+        }
       })
       .catch((e) => {
+        handleProxyResponse(e);
         console.log({ e });
       });
   }, [currentTeam]);
   return (
-    <section>
-      {items.length <= 0 ? (
-        <div>
-          <CenteredLayout>
-            <>
-              <TitleDescriptionLink title={__("No Data")} />
-            </>
-          </CenteredLayout>
-        </div>
+    <>
+      {!showLogin ? (
+        <>
+          {modalOpen ? (
+            <Modal
+              isOpen={true}
+              Logo={() => <div> Logo?</div>}
+              setIsOpen={setModalTeam}
+              title={__("Invite New Team Member", "trustedlogin-vendor")}>
+              <InviteMember team={currentTeam} />
+            </Modal>
+          ) : null}
+          <section>
+            {items.length <= 0 ? (
+              <div>
+                <CenteredLayout>
+                  <>
+                    <TitleDescriptionLink
+                      title={hasLoaded ? "Loading" : __("No Team Data Found")}
+                    />
+                  </>
+                </CenteredLayout>
+              </div>
+            ) : (
+              <div>
+                <TablePage
+                  title={"Admin Team"}
+                  subTitle={"Manage your team"}
+                  items={items}
+                  SearchArea={() => (
+                    <Fragment>
+                      <PrimaryButton onClick={() => setModalTeam(true)}>
+                        {__("Invite Team Member")}
+                      </PrimaryButton>
+                    </Fragment>
+                  )}
+                  ActionArea={(item) => (
+                    <Fragment key={item.id}>
+                      <ActionItemButton isRed={false}>Button</ActionItemButton>
+                    </Fragment>
+                  )}
+                />
+              </div>
+            )}
+          </section>
+        </>
       ) : (
-        <div>
-          <TablePage
-            title={"Admin Team"}
-            subTitle={"Manage your team"}
-            items={items}
-            SearchArea={() => (
-              <Fragment>
-                <PrimaryButton onClick={() => setModalOpen(true)}>
-                  {__("Invite Team Member")}
-                </PrimaryButton>
-              </Fragment>
-            )}
-            ActionArea={(item) => (
-              <Fragment key={item.id}>
-                <ActionItemButton isRed={false}>Button</ActionItemButton>
-              </Fragment>
-            )}
-          />
-        </div>
+        <>
+          <LoginOrLogout />
+        </>
       )}
-    </section>
+    </>
   );
 }
