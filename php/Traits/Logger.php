57059e7c7f7e088a1bc3ab1ded17ce421558ae86
Zack Katz <zack@katz.co>
2023-02-27 23:27:41 -0500

    Fix PHP warnings caused by deprecated implicit conversion
    PHP Deprecated:  Implicit conversion from float 1677558004.236047 to int loses precision in /wp-content/plugins/vendor/php/Traits/Logger.php on line 78

--- php/Traits/Logger.php
+++ php/Traits/Logger.php
@@ -74,7 +74,7 @@ trait Logger
     {
         $originalTime = microtime(true);
         $micro = sprintf("%06d", ($originalTime - floor($originalTime)) * 1000000);
-        $date = new DateTime(date('Y-m-d H:i:s.'.$micro, $originalTime));
+        $date = new DateTime( date( 'Y-m-d H:i:s.' . $micro, (int) $originalTime ) );
 
         return $date->format('Y-m-d H:i:s');
     }
