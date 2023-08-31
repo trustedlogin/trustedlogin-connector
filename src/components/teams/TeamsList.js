81126a5ddd6e8578dec423e9f6f3f0de2a112bfe
Zack Katz <zack@katz.co>
2023-08-31 15:14:18 -0400

    Remove unused teams search input

--- src/components/teams/TeamsList.js
+++ src/components/teams/TeamsList.js
@@ -97,34 +97,6 @@ const TeamsList = () => {
             subTitle={__("Manage your TrustedLogin settings", "trustedlogin-vendor")}
             Button={() => (
               <>
-                <div>
-                  <label htmlFor="search" className="sr-only">
-                    {__("Search", "trustedlogin-vendor")}
-                  </label>
-                  <div className="relative h-full">
-                    <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                      <svg
-                        className="h-5 w-5 text-gray-400"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                        aria-hidden="true">
-                        <path
-                          fillRule="evenodd"
-                          d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
-                    </div>
-                    <input
-                      id="search"
-                      name="search"
-                      className="block w-full h-full !pl-10 !pr-3 !py-3 !border !border-gray-300 !rounded-lg leading-5 !bg-white placeholder-gray-500 focus:outline-none focus:ring-2 ring-offset-2 focus:ring-sky-500 sm:text-sm sm:py-2"
-                      placeholder={__('Search…', 'trustedlogin-vendor')}
-                      type="search"
-                    />
-                  </div>
-                </div>
                 <PrimaryButton onClick={() => setCurrentView("teams/new")}>
                   <>
                     <svg
