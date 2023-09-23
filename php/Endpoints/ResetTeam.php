c0956d609255efcaca15c4f7c07c60a174543f2f
Daniel Iser <daniel@code-atlantic.com>
2023-09-22 23:19:48 -0400

    Fix incorrect class override

--- php/Endpoints/ResetTeam.php
+++ php/Endpoints/ResetTeam.php
@@ -30,7 +30,8 @@ class ResetTeam extends Settings
 		];
 	}
 
-	public function get(\WP_REST_Request $request){
+	public function get(\WP_REST_Request $request)
+	{
 		$settingsApi = SettingsApi::fromSaved();
 		return $this->createResponse($settingsApi);
 	}
@@ -44,22 +45,20 @@ class ResetTeam extends Settings
 	public function update(\WP_REST_Request $request)
 	{
 		$settingsApi = SettingsApi::fromSaved();
-		if ( in_array( [ 'helpscout', 'freescout' ], $request->get_param( 'integration' ), true ) ) {
+		if (in_array([ 'helpscout', 'freescout' ], $request->get_param('integration'), true)) {
 			try {
-				$settingsApi = $settingsApi->getByAccountId( $request->get_param('accountId') );
+				$account = $settingsApi->getByAccountId($request->get_param('accountId'));
 				$settingsApi->resetHelpdeskSettings(
 					$request->get_param('accountId'),
 					$request->get_param('integration')
 				);
-
 			} catch (\Throwable $th) {
-				return rest_ensure_response( [
+				return rest_ensure_response([
 					'error' => 'Account not found'
-				] );
+				]);
 			}
 		}
 
-		return $this->createResponse( SettingsApi::fromSaved());
+		return $this->createResponse(SettingsApi::fromSaved());
 	}
-
 }
