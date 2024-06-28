ff6648d47aa69e6b2772184c7a439df8d88d20cb
Marc Gratch <me@marcgratch.com>
2024-06-28 18:36:13 -0500

    Issue #169 Handle Add/Update Team errors better & Loading States.

--- src/components/teams/TeamsList.js
+++ src/components/teams/TeamsList.js
@@ -1,4 +1,4 @@
-import { useMemo, useState, Fragment } from "react";
+import { useMemo, useState, Fragment, useEffect } from "react";
 import { useSettings } from "../../hooks/useSettings";
 import { useView } from "../../hooks/useView";
 import { PrimaryButton, SubmitAndCancelButtons } from "../Buttons";
@@ -6,20 +6,19 @@ import { ConfigureHelpDesk } from "../integrations/ConfigureIntegration";
 import { CenteredLayout, PageHeader } from "../Layout";
 import TitleDescriptionLink from "../TitleDescriptionLink";
 import { __, _x } from "@wordpress/i18n";
-/**
- * Show list of teams
- *
- * @returns {JSX.Element}
- */
+import Spinner from "../Spinner";
+import { ToastError } from "../Errors";
+
 const TeamsList = () => {
-  const { settings, removeTeam } = useSettings();
-  //Has user clicked delete, but not confirmed?
+  const { settings, removeTeam, loading } = useSettings();
   const [isDeleting, setIsDeleting] = useState(false);
-  //Track team to delete, if confirmed.
   const [teamDeleting, setTeamDeleting] = useState(null);
   const { setCurrentView, setCurrentTeam } = useView();
   const teams = useMemo(() => settings.teams, [settings]);
   const [modalTeam, setModalTeam] = useState(null);
+  const [errorMessages, setErrorMessages] = useState([]);
+  const [previousLoading, setPreviousLoading] = useState(false);
+
   const shouldShowConfigureButton = (team) => {
     return (
       team.helpdesk &&
@@ -27,70 +26,58 @@ const TeamsList = () => {
     );
   };
 
-  /**
-   * Cancel delete process
-   */
-  function cancelDelete() {
+  const cancelDelete = () => {
     setIsDeleting(false);
     setTeamDeleting(null);
-  }
+  };
 
-  /**
-   * Completes the deletion of a team
-   */
-  function completeDelete() {
+  const completeDelete = () => {
     removeTeam(teamDeleting, () => {
       cancelDelete();
     });
-  }
+  };
 
-  /**
-   * Displays the confirmation and stores ID of team to be deleted
-   */
-  function startDelete(teamId) {
+  const startDelete = (teamId) => {
     setIsDeleting(true);
     setTeamDeleting(teamId);
-  }
+  };
 
-  function goToAccessKey(teamId) {
+  const goToAccessKey = (teamId) => {
     setCurrentTeam(teamId);
     setCurrentView("teams/access_key");
-  }
+  };
+
+  useEffect(() => {
+    if (previousLoading && !loading) {
+      const errors = teams.filter((team) => team.status === "error");
+      if (errors.length > 0) {
+        setErrorMessages(
+          errors.map((team) => {
+            return {
+              message: team.message,
+              id: team.id,
+              name:
+                team.name !== ""
+                  ? team.name
+                  : __("Team", "trustedlogin-connector") + " " + team.id,
+              account_id: team.account_id,
+            };
+          })
+        );
+      }
+    }
+    setPreviousLoading(loading);
+  }, [loading, previousLoading, teams]);
 
   return (
     <>
-      <>
-        {teams.map((team) => {
-          // Destructure id and helpdesk from team, provide default values in case they're undefined
-          const { id = null, helpdesk = [] } = team;
-
-          // Get the first helpdesk, or "helpscout" if helpdesk is an empty array
-          const firstHelpDesk =
-            Array.isArray(helpdesk) && helpdesk.length > 0
-              ? helpdesk[0]
-              : "helpscout";
-
-          return (
-            <Fragment key={id}>
-              <ConfigureHelpDesk
-                isOpen={modalTeam === id}
-                setIsOpen={() => {
-                  setModalTeam(null);
-                }}
-                team={team}
-                helpDesk={firstHelpDesk}
-              />
-            </Fragment>
-          );
-        })}
-      </>
+      {loading && <Spinner size={150} />}
       {isDeleting ? (
         <CenteredLayout>
           <>
             <TitleDescriptionLink
               title={__("Are You Sure?", "trustedlogin-connector")}
             />
-
             <SubmitAndCancelButtons
               onSubmit={completeDelete}
               submitText={__("Delete Team", "trustedlogin-connector")}
@@ -136,75 +123,117 @@ const TeamsList = () => {
           <div className="flex flex-col justify-center w-full bg-white rounded-lg shadow">
             <ul role="list" className="divide-y divide-gray-200 px-5 sm:px-8">
               {teams.map((team) => {
+                const { id = null, helpdesk = [], status, message } = team;
+                const firstHelpDesk =
+                  Array.isArray(helpdesk) && helpdesk.length > 0
+                    ? helpdesk[0]
+                    : "helpscout";
+
                 return (
-                  <li
-                    key={team.id}
-                    className="py-5 flex flex-col items-center justify-between sm:py-8 sm:flex-row">
-                    <div className="flex w-full items-center space-x-5 sm:w-auto">
-                      <button
-                        onClick={() => goToAccessKey(team.id)}
-                        className="flex-shrink-0 flex items-center justify-center h-12 w-12 bg-purple-600 text-white text-sm font-medium rounded-lg">
-                        {team.name ? team.name.substring(0, 2) : "TL"}
-                      </button>
-                      <div className="flex flex-row space-x-16 items-center w-full justify-between sm:justify-start">
-                        <div className="flex flex-col max-w-[10rem] sm:max-w-[8rem] md:max-w-none">
-                          <p
-                            className="text-lg font-medium text-gray-900 leading-tight min-w-[6rem]"
-                            id="team-option-1-label">
-                            {team.name
-                              ? team.name
-                              : _x(
-                                  "Team {id}",
-                                  "{id} is replaced dynamically; do not translate",
-                                  "trustedlogin-connector"
-                                ).replace("{id}", team.id)}
-                          </p>
-                          <p
-                            className="text-sm text-gray-500"
-                            id="team-option-1-description">
-                            #{team.account_id}
-                          </p>
+                  <Fragment key={id}>
+                    <ConfigureHelpDesk
+                      isOpen={modalTeam === id}
+                      setIsOpen={() => {
+                        setModalTeam(null);
+                      }}
+                      team={team}
+                      helpDesk={firstHelpDesk}
+                    />
+                    <li className="py-5 flex flex-col items-center justify-between sm:py-8 sm:flex-row">
+                      <div className="flex w-full items-center space-x-5 sm:w-auto">
+                        <button
+                          onClick={() => goToAccessKey(team.id)}
+                          className="flex-shrink-0 flex items-center justify-center h-12 w-12 bg-purple-600 text-white text-sm font-medium rounded-lg">
+                          {team.name ? team.name.substring(0, 2) : "TL"}
+                        </button>
+                        <div className="flex flex-row space-x-16 items-center w-full justify-between sm:justify-start">
+                          <div className="flex flex-col sm:flex-row sm:items-center max-w-[10rem] sm:max-w-[8rem] md:max-w-none">
+                            <div className="flex flex-col">
+                              <p
+                                className="text-lg font-medium text-gray-900 leading-tight min-w-[6rem]"
+                                id="team-option-1-label">
+                                {team.name
+                                  ? team.name
+                                  : _x(
+                                      "Team {id}",
+                                      "{id} is replaced dynamically; do not translate",
+                                      "trustedlogin-connector"
+                                    ).replace("{id}", team.id)}
+                              </p>
+                              <p
+                                className="text-sm text-gray-500"
+                                id="team-option-1-description">
+                                #{team.account_id}
+                              </p>
+                            </div>
+                            {status === "error" && (
+                              <>
+                                <div
+                                  className="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4 ml-4"
+                                  role="alert">
+                                  <p className="font-bold">
+                                    {__(
+                                      "Oops something went wrong",
+                                      "trustedlogin-connector"
+                                    )}
+                                    .
+                                  </p>
+                                  <p>{message}.</p>
+                                </div>
+                              </>
+                            )}
+                          </div>
                         </div>
                       </div>
-                    </div>
-                    <div className="flex items-center space-x-5 w-full mt-4 justify-between sm:w-auto sm:mt-0">
-                      <div className="flex items-center space-x-6">
-                        {shouldShowConfigureButton(team) ? (
+                      <div className="flex items-center space-x-5 w-full mt-4 justify-between sm:w-auto sm:mt-0">
+                        <div className="flex items-center space-x-6">
+                          {shouldShowConfigureButton(team) ? (
+                            <button
+                              onClick={() => {
+                                setModalTeam(team.id);
+                              }}
+                              className="text-sm text-blue-tl hover:text-navy-tl p-2">
+                              {__(
+                                "Configure Help Desk",
+                                "trustedlogin-connector"
+                              )}
+                            </button>
+                          ) : null}
+
                           <button
                             onClick={() => {
-                              setModalTeam(team.id);
+                              setCurrentView("teams/edit");
+                              setCurrentTeam(team.id);
                             }}
                             className="text-sm text-blue-tl hover:text-navy-tl p-2">
-                            {__(
-                              "Configure Help Desk",
-                              "trustedlogin-connector"
-                            )}
+                            {__("Edit", "trustedlogin-connector")}
                           </button>
-                        ) : null}
-
-                        <button
-                          onClick={() => {
-                            setCurrentView("teams/edit");
-                            setCurrentTeam(team.id);
-                          }}
-                          className="text-sm text-blue-tl hover:text-navy-tl p-2">
-                          {__("Edit", "trustedlogin-connector")}
-                        </button>
-                        <button
-                          onClick={() => startDelete(team.id)}
-                          className="text-sm text-red-500 hover:text-red-800 p-2">
-                          {__("Delete", "trustedlogin-connector")}
-                        </button>
+                          <button
+                            onClick={() => startDelete(team.id)}
+                            className="text-sm text-red-500 hover:text-red-800 p-2">
+                            {__("Delete", "trustedlogin-connector")}
+                          </button>
+                        </div>
                       </div>
-                    </div>
-                  </li>
+                    </li>
+                  </Fragment>
                 );
               })}
             </ul>
           </div>
         </div>
       )}
+      {!loading &&
+        errorMessages.map((error, index) => (
+          <ToastError
+            key={index}
+            heading={"Oops something went wrong with " + error.name}
+            text={error.message}
+            isDismissible={true}
+          />
+        ))}
     </>
   );
 };
+
 export default TeamsList;
